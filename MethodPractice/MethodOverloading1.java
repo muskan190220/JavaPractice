ee6432a28c47b846131ff747c9839728f890ab31
muskan <muskanbharatkesharwani@gmail.com>
2025-01-04 17:10:54 +0530

    Add new Program

---
+++ MethodPractice/MethodOverloading1.java
@@ -0,0 +1,25 @@
+package MethodPractice;
+
+public class MethodOverloading1 
+{
+    static int area(int l, int b)
+    {
+        int a = l*b;
+        return a;
+    }
+    static float area(float r)
+    {
+        float c = (float) ((3.14)*r*r);
+        return c;
+    }
+    static int area(int l)
+    {
+        int s = l*l;
+        return s;
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println(area(3));
+    }
+}
