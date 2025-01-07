108ac701b0ad0ec3e79829306e587be2835faf34
muskan <muskanbharatkesharwani@gmail.com>
2025-01-07 21:49:36 +0530

    Add New Folder

---
+++ DataHiding/Rectangle.java
@@ -0,0 +1,49 @@
+package DataHiding;
+class Rectangle {
+    private int length;
+    private int breadth;
+    
+
+    //set length
+    public int getLength()
+    {
+        return length;
+    }
+    public void setLength(int l)
+    {
+        if(l>0)
+        {
+            length = l;
+        }
+        else
+        {
+            length = 0;
+        }
+    }
+    //set breadth
+    public int getBreadth()
+    {
+        return breadth;
+    }
+    public void setBreadth(int b)
+    {
+        if(b>0)
+        {
+            breadth = b;
+        }
+        else
+        {
+            breadth = 0;
+        }
+    }
+
+    public int area()
+    {
+        return length*breadth;
+    }
+    public int perimeter()
+    {
+        return 2*(length+breadth);
+    }
+     
+}
---
+++ DataHiding/Rectangletest.java
@@ -0,0 +1,14 @@
+package DataHiding;
+
+public class Rectangletest {
+    public static void main(String args[])
+    {
+        Rectangle r = new Rectangle();
+        r.setLength(10);
+        r.setBreadth(15);
+        
+        System.out.println(r.area());
+        System.out.println(r.perimeter());
+
+    }
+}
