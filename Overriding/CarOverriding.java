ec4310454bab5d4ba107a9f1fbddfe93f68a0cfc
muskan <muskanbharatkesharwani@gmail.com>
2025-01-24 18:50:33 +0530

    Add New Program

---
+++ Overriding/CarOverriding.java
@@ -0,0 +1,53 @@
+package Overriding;
+
+class Car
+{
+    public void Start()
+    {
+        System.out.println("Car started");
+    }
+    public void Accelerate()
+    {
+        System.out.println("Accelerating the car");
+    }
+    public void ChangeGear()
+    {
+        System.out.println("Changing the gear Manually");
+    }
+}
+class LuxuryCar
+{
+    public void Start()
+    {
+        System.out.println("Car started");
+    }
+    public void Accelerate()
+    {
+        System.out.println("Accelerating the car");
+    }
+    public void ChangeGear()
+    {
+        System.out.println("Changing the gear Automatically");
+    }
+    public void OpenRoof()
+    {
+        System.out.println("Sunroof getting opened");
+    }
+}
+
+public class CarOverriding {
+    public static void main(String args[])
+    {
+        Car c = new Car();
+        c.Start();
+        c.ChangeGear();
+        c.Accelerate();
+
+        LuxuryCar lc = new LuxuryCar();
+        lc.Start();
+        lc.Accelerate();
+        lc.ChangeGear();
+        lc.OpenRoof();
+
+        
+}}
