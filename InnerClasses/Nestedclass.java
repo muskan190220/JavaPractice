9066ac49d505adf9fa0910b65aea392ea6191788
muskan <muskanbharatkesharwani@gmail.com>
2025-02-01 20:40:33 +0530

    Add New Program

---
+++ InnerClasses/Nestedclass.java
@@ -0,0 +1,38 @@
+package InnerClasses;
+
+class Outer
+{
+    int x = 10;
+    class Inner
+    {
+        int y = 20;
+        void swap()
+        {
+            System.out.println("Old values: ");
+            System.out.println("x = "+x);
+            System.out.println("y = "+y);
+            
+            x = x+y;
+            y = x-y;
+            x = x-y;
+
+            System.out.println("New Values: ");
+            System.out.println("x = "+x);
+            System.out.println("y = "+y);
+        }
+    }
+    void Answer()
+    {
+        Inner i = new Inner();
+        i.swap();
+    }
+}
+public class Nestedclass 
+{
+    public static void main(String args[])
+    {
+        Outer o = new Outer();
+        o.Answer();
+    }
+    
+}
