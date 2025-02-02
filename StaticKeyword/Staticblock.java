01432965d0f727ebe48f08ec8ef77f3a74ddf4f2
muskan <muskanbharatkesharwani@gmail.com>
2025-02-02 15:00:19 +0530

    Add New Program

---
+++ StaticKeyword/Staticblock.java
@@ -0,0 +1,21 @@
+package StaticKeyword;
+
+public class Staticblock
+{
+    static
+    {
+        System.out.println("Block1");
+    }
+    static
+    {
+        System.out.println("Block 2");
+    }
+
+
+    public static void main(String args[]) 
+    {
+        System.out.println("Main");
+        
+
+    }   
+}
