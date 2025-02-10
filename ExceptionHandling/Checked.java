6a0657af1129f9323807a12337f6cfc9c70fbb1e
muskan <muskanbharatkesharwani@gmail.com>
2025-02-10 15:28:00 +0530

    Make Some Changes in Program Checked

--- ExceptionHandling/Checked.java
+++ ExceptionHandling/Checked.java
@@ -1,18 +1,33 @@
 package ExceptionHandling;
 import java.io.*;
 
-
+class lowbalexception extends Exception{
+    public String toString()
+    {
+        return "Balance should be more than 5000";
+    }
+}
 public class Checked {
     
         static void fun1()
         {
-            try{
-                FileInputStream f = new FileInputStream("my.txt");
+            try {
+                throw new lowbalexception();
             }
-            catch(FileNotFoundException e)
+            catch(lowbalexception e)
             {
-                System.out.println("File not found");
+                System.out.println(e);
             }
         }
-    }
+        static void fun2()
+        {
+            fun1();
+        }
+        public static void main(String args[])
+        {
+            
+            
+            Checked.fun2();
+        }
+}
 
