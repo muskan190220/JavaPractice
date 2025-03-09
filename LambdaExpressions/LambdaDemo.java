3486a2fa9962a6687f62b7b2bc8df6f8fd0a4203
muskan <muskanbharatkesharwani@gmail.com>
2025-03-10 01:29:46 +0530

    Add LambdaExpressions file

---
+++ LambdaExpressions/LambdaDemo.java
@@ -0,0 +1,18 @@
+package LambdaExpressions;
+
+@FunctionalInterface
+interface MyLambda
+{
+    public void display();
+}
+public class LambdaDemo 
+{
+    public static void main(String[] args) 
+    {
+        MyLambda m = () ->
+        {
+            System.out.println("Hello World");
+        };
+        m.display();
+    }
+}
