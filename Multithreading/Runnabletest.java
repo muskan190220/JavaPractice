5d2cb9e94b1848f7bcc46e48eeb1d4bfd1caec7b
muskan <muskanbharatkesharwani@gmail.com>
2025-03-17 22:23:10 +0530

    Add New Program

---
+++ Multithreading/Runnabletest.java
@@ -0,0 +1,31 @@
+package Multithreading;
+
+class My implements Runnable
+{
+    public void run()
+    {
+        int i = 1;
+        while(true)
+        {
+            System.out.println(i+ "Hello");
+            i++;
+        }
+    }   
+}
+public class Runnabletest 
+{
+    public static void main(String args[])
+    {
+        My m = new My();
+        Thread t = new Thread(m);
+        t.start();
+
+        int i = 1;
+        while(true)
+        {
+            System.out.println(i+"World");
+            i++;
+        }
+    }
+    
+}
